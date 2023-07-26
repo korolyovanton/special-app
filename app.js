a81b7afa83e80766019322b432e5f08d8cb131fb
Anton Korolyov <antonkor12@gmail.com>
2023-07-26 18:06:52 +0300

    new pars

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ $(function(){
     let button = document.querySelector('.submit');
     let pricesBlock = document.querySelector('.prices');
     pricesBlock.style.backgroundColor = 'green';
+    pricesBlock.style.backgroundColor = 'green';
+    pricesBlock.style.backgroundColor = 'green';
 });
 
 mmmm
