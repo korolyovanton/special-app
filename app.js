71cf91ebf0129c526c81895f794d63ce737279fb
Anton Korolyov <antonkor12@gmail.com>
2023-07-26 17:39:26 +0300

    new commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(function(){
-    let button = document.querySelector('.submit');
+    let button = document.querySelector('.submittt');
     let pricesBlock = document.querySelector('.prices');
     pricesBlock.style.backgroundColor = 'green';
 });
